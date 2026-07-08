03cbcfa717e87067c6ceb1eb1510cf325cc2829b
hian128 <hiangamer725@gmail.com>
2026-07-07 22:08:12 -0300

    primeiro commit do site

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// Rolagem suave
+    document.querySelectorAll('a[href^="#"]').forEach(link => {
+
+        link.addEventListener('click', function(e){
+
+            e.preventDefault();
+
+            document.querySelector(this.getAttribute('href'))
+            .scrollIntoView({
+
+                behavior:'smooth'
+
+            });
+
+        });
+
+    });
